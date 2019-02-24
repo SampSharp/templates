c6af298ba1aed8e608e0f2f5840dc1540e6e50c3
Tim Potze <timpotze@gmail.com>
2019-02-24 17:28:03 +0100

    Added initial files

---
+++ MyGameMode/Program.cs
@@ -0,0 +1,14 @@
+﻿using SampSharp.Core;
+
+namespace MyGameMode
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			new GameModeBuilder()
+				.Use<GameMode>()
+				.Run();
+		}
+	}
+}
