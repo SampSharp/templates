7ebf1aaccbdea6d6c01008c8093c93250596ed43
Tim Potze <timpotze@gmail.com>
2020-01-28 19:12:36 +0100

    Updated templates to include ECS template

---
+++ MyEcsGameMode/Program.cs
@@ -0,0 +1,15 @@
+﻿using SampSharp.Core;
+using SampSharp.Entities;
+
+namespace MyEcsGameMode
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			new GameModeBuilder()
+				.UseEcs<Startup>()
+				.Run();
+		}
+	}
+}
